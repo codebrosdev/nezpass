eb9870126c9313587402d432c64bb7f1d2f5b819
Kuya Joe <hey@kuyajoe.dev>
2023-01-29 04:13:42 +0000

    update notices

--- resources/views/shared/notices.blade.php
+++ resources/views/shared/notices.blade.php
@@ -1,5 +1,3 @@
-<p>We use <strong>cookies</strong> to store some data.  We only use this so user does not have to enter everything again. By using this site, you agree to the use of cookies.</p>
-<p style="color:red">The cookies expire within a day.</p>
-
-<p>It is probably a good idea to have additional secret characters that you append or prepend to the generated password. An example is to add special characters at the end of the password and use that. This way, even if someone snooped while you were generating the password, they would have to guess the additional characters that you only know (unless they have a keylogger)</p>
+<p>We store some data using <span style="color: red; font-weight:600">cookies</span> to avoid the need for users to re-enter information. By accessing this site, you consent to the use of cookies. Don&rsquo;t worry - cookies will expire within a day</p>
+<p>It's advisable to add secret characters to the generated password, either by appending or prepending them. For example, adding special characters to the end of the password makes it more secure, as it would be harder for someone to guess them if they managed to snoop on the generation process, unless they have a keylogger.</p>
 
