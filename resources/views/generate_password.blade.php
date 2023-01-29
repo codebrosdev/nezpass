f6afece81a290f5cb4dfc6d77920403ce64ff859
Kuya Joe <hey@kuyajoe.dev>
2023-01-29 04:06:43 +0000

    shorten paragraph and fix the form a bit

--- resources/views/generate_password.blade.php
+++ resources/views/generate_password.blade.php
@@ -3,17 +3,20 @@
 <title>NezPass: Ce NezPass (ne pas) LessPass</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script defer src="https://unpkg.com/alpinejs@3.2.4/dist/cdn.min.js"></script>
+
 </head>
 
 <body>
 <div class="container">
-
+<div class="col-md-6">
 <h1>NezPass: LessPass with a database</h1>
 
-<p>NezPass may be less secure than LessPass.com because the form submits to the server and returns back the generated password. We employ the same algorithm used by LessPass.com but the inputs actually enter the server. We only store the master password and the password criteria for subsequent use.</p>
+<p>We use the same algorithm as LessPass.com and input the password criteria and master password on our server to generate a password, which we store for future use.</p>
 
 @include('shared.notices')
-
+</div>
+<div class="col-md-6">
 <form action="{{ route('generate_password') }}" method="POST">
     @csrf
 <div>
@@ -40,19 +43,32 @@
 @endif
 </div>
 
-<div>
-	<input type="checkbox" name="use_lowercase" {{ ($data && $data->lowercase) ? 'checked' : '' }}><label for="use_lowercase">Use lowercase</label>
-	<input type="checkbox" name="use_uppercase" {{ ($data && $data->uc)  ? 'checked': '' }}><label for="use_uppercase">Use uppercase</label>
-	<input type="checkbox" name="use_symbols" {{ ($data && $data->symbols) ? 'checked' : '' }}><label for="use_symbols">Use symbols</label>
-	<input type="checkbox" name="use_numbers" {{ ($data && $data->numbers) ? 'checked' : '' }}><label for="use_numbers">Use numbers</label>
-</div>
-<br />
-<input type="reset" value="clear" style="background-color: orange">
+<div style="border: 1px solid grey; padding-left:15px; padding-top:5px;">
+	<div class="input-group">
+      <input type="checkbox" aria-label="Checkbox to use lowercase" name="use_lowercase" {{ ($data && $data->lowercase) ? 'checked' : '' }} style="width: 30px; height: 30px;"><label for="use_lowercase" class="input-group-text">Use lowercase (a-z)</label>
+     </div>
 
-<input style="border:2px solid lightblue;" type="submit" name="submit" value="generate">
+	<div class="input-group">
+	<input type="checkbox" name="use_uppercase" {{ ($data && $data->uc)  ? 'checked': '' }} style="width: 30px; height: 30px;"><label for="use_uppercase" class="input-group-text">Use uppercase (A-Z)</label>
+	</div>
+
+	<div class="input-group">
+<input type="checkbox" name="use_numbers" {{ ($data && $data->numbers) ? 'checked' : '' }} style="width: 30px; height: 30px;"><label for="use_numbers" class="input-group-text">Use numbers (0-9)</label>
+    </div>
+
+	<div class="input-group">
+	<input type="checkbox" name="use_symbols" {{ ($data && $data->symbols) ? 'checked' : '' }} style="width: 30px; height: 30px;"><label for="use_symbols" class="input-group-text">Use symbols (%!@)</label>
+	</div>
+<br />
+<input type="reset" value="clear" style="background-color: grey; color: darkblue">
+
+<input style="border:2px solid blue;background: green; color: white" type="submit" name="submit" value="generate">
 
 </form>
+<br><br>
 </div>
+</div>
+
 
 </body>
 </html>
