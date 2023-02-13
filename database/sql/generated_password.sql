0cf1406a11f297e3008c49ec2cc3d80a5a474f56
Kuya Joe <hey@kuyajoe.dev>
2023-02-13 15:36:51 +0800

    Update generated_password.sql

--- database/sql/generated_password.sql
+++ database/sql/generated_password.sql
@@ -1,2 +1,4 @@
-CREATE TABLE generated_passwords ( uuid VARCHAR(36), session_id VARCHAR(25), created_at DATETIME DEFAULT '2000-01-01 00:00:00', deleted TINYI
-NT(1) DEFAULT '0')
+CREATE TABLE generated_passwords ( uuid VARCHAR(36), session_id VARCHAR(25), 
+                                  created_at DATETIME DEFAULT '2000-01-01 00:00:00', 
+                                  delete_by DATETIME,
+                                  deleted TINYINT(1) DEFAULT '0')
